4ff196e769c5346d16c55fe0a60e22670ebd217f
MarceloCarvalhoRJ <117359391+MarceloCarvalhoRJ@users.noreply.github.com>
2023-02-27 12:30:46 +0000

    acerto

--- Scripts/pythonexercicios/desafio_57_while.py
+++ Scripts/pythonexercicios/desafio_57_while.py
@@ -1,6 +1,6 @@
 sexo = ''
 while sexo != 'M' and sexo != 'F':
-    sexo = input('Digite seu sexo [M/F]: ').strip().upper()[0]m
+    sexo = input('Digite seu sexo [M/F]: ').strip().upper()[0]
     print('Opção inválida, digite M para Masculino ou F para feminino.')
 if sexo == 'M':
     print('O Sexo escohido foi Masculino!')
